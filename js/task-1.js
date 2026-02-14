f2fa54f802618ea8da32a4c78e129e264d6bcabc
Alina Kozlyuk <talina787@icloud.com>
2026-02-14 14:18:10 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,17 @@
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
-}
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesItems.length}`);
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+categoriesItems.forEach(item => {
+    const titleElement = item.querySelector('h2'); // як знайти h2?
+    const title = titleElement.textContent; // як отримати текст?
+
+    const elems = item.querySelectorAll('li');
+    const elemCount = elems.length;
+
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${elemCount}`);
+});
+
+
+
